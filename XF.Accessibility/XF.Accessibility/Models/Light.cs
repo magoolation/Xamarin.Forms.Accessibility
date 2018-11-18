1500668cb5d17453aef54ec5c1a9fbe8df31cb19
Alexandre Santos Costa <alexandresantoscosta@gmail.com>
2018-11-18 18:36:02 -0200

    Playing with some XF 3.x features

---
+++ XF.Accessibility/XF.Accessibility/Models/Light.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF.Accessibility.Models
+{
+    public class Light
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+
+        public static Light Red => new Light() { Name = "Red", Color = Color.Red };
+        public static Light Yellow => new Light() { Name = "Yellow", Color = Color.Yellow };
+        public static Light Green => new Light() { Name = "Green", Color = Color.Green};
+    }
+}
---
+++ XF.Accessibility/XF.Accessibility/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Prism.Navigation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF.Accessibility.Models;
+
+namespace XF.Accessibility.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private List<Light> lights;
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            lights = new List<Light>() { Light.Red, Light.Yellow, Light.Green };
+
+            SwipeLeftCommand = new Command(SwipeLightLeft);
+            SwipeRightCommand = new Command(SwipeLightRight);
+        }
+
+        private void SwipeLightLeft()
+        {
+            SwitchLeft();
+            RaisePropertyChanged(nameof(Lights));
+        }
+
+        private void SwipeLightRight()
+        {
+            SwitchRight();
+            RaisePropertyChanged(nameof(Lights));
+        }
+
+        private void Swap(int x, int y)
+        {
+            var actual = lights[x];
+            lights[x] = lights[y];
+            lights[y] = actual;
+        }
+
+        private void SwitchLeft()
+        {
+            lights.Add(lights[0]);
+            lights.RemoveAt(0);
+        }
+
+        private void SwitchRight()
+        {
+            lights.Insert(0, lights.Last());
+            lights.RemoveAt(lights.Count() - 1);
+        }
+
+        public List<Light> Lights
+        {
+            get => lights;
+            set => SetProperty(ref lights, value);
+        }
+
+        public ICommand SwipeLeftCommand { get; }
+        public ICommand SwipeRightCommand { get; }
+    }
+}
